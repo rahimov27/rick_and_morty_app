909dd380a313174c3c1ee700c404ed6036f8935c
rahimov27 <89564054+rahimov27@users.noreply.github.com>
2024-10-21 01:22:05 +0600

    character detailed page ux

--- lib/presentation/screens/character_details_page.dart
+++ lib/presentation/screens/character_details_page.dart
@@ -18,32 +18,127 @@ class CharacterDetailsPage extends StatelessWidget {
       body: SafeArea(
         child: Stack(
           children: [
-            Column(
-              children: [
-                Stack(
-                  children: [
-                    Image.network(
-                      width: double.infinity,
-                      fit: BoxFit.cover,
-                      height: 300,
-                      "${data.image}",
-                    ),
-                    Container(
-                      height: 300,
-                      color: Colors.black
-                          .withOpacity(0.2), // затемнение изображения
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 100), // для пространства под аватаром
-                Text(
-                  data.name ?? 'Character Name',
-                  style: const TextStyle(
-                      fontSize: 34,
-                      fontWeight: FontWeight.w400,
-                      color: AppColors.white),
-                ),
-              ],
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  Stack(
+                    children: [
+                      Image.network(
+                        width: double.infinity,
+                        fit: BoxFit.cover,
+                        height: 300,
+                        "${data.image}",
+                      ),
+                      Container(
+                        height: 300,
+                        color: Colors.black
+                            .withOpacity(0.2), // затемнение изображения
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 100), // для пространства под аватаром
+                  Column(
+                    children: [
+                      Text(
+                        data.name ?? 'Character Name',
+                        style: const TextStyle(
+                            fontSize: 34,
+                            fontWeight: FontWeight.w400,
+                            color: AppColors.white),
+                      ),
+                      Text(
+                        data.status?.toUpperCase() ?? 'Character Status',
+                        style: const TextStyle(
+                            fontSize: 10,
+                            fontWeight: FontWeight.w500,
+                            color: AppColors.green),
+                      ),
+                      const SizedBox(height: 30),
+                      const Text(
+                        "Главный протагонист мультсериала «Рик и Морти». Безумный ученый, чей алкоголизм, безрассудностьи социопатия заставляют беспокоиться семью его дочери.",
+                        style: TextStyle(
+                            height: 1.2,
+                            fontSize: 13,
+                            fontWeight: FontWeight.w400,
+                            color: AppColors.white),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 30),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            "Пол",
+                            style: TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.w400,
+                                color: AppColors.grey),
+                          ),
+                          Text(
+                            data.gender ?? 'Character Gender',
+                            style: const TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.w400,
+                                color: AppColors.white),
+                          ),
+                        ],
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            "Расса",
+                            style: TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.w400,
+                                color: AppColors.grey),
+                          ),
+                          Text(
+                            data.species ?? 'Character Species',
+                            style: const TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.w400,
+                                color: AppColors.white),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(width: 0),
+                    ],
+                  ),
+                  const SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            "Место рождения",
+                            style: TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.w400,
+                                color: AppColors.grey),
+                          ),
+                          Text(
+                            data.origin?.name ?? 'Character Origin',
+                            style: const TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.w400,
+                                color: AppColors.white),
+                          ),
+                        ],
+                      ),
+                      const Icon(Icons.arrow_forward_ios_rounded)
+                    ],
+                  ),
+                ],
+              ),
             ),
             Positioned(
               top: 210, // корректное позиционирование аватара

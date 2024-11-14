b4289912cfc9b8310b47af079c29f9633a44bf17
rahimov27 <89564054+rahimov27@users.noreply.github.com>
2024-11-14 14:17:07 +0600

    settings-page:feat added sharedprefs for saving user ceo

--- lib/features/profile/presentation/widgets/edit_button_widget.dart
+++ lib/features/profile/presentation/widgets/edit_button_widget.dart
@@ -2,23 +2,34 @@ import 'package:flutter/material.dart';
 import 'package:rick_and_morty_app/shared/theme/app_colors.dart';
 
 class EditButtonWidget extends StatelessWidget {
+  final String? buttonText;
   final VoidCallback onPressed;
-  const EditButtonWidget({
-    super.key,
-    required this.onPressed
-  });
+  final bool isFilled = false;
+  final Color? bgColor;
+  final Color? textColor;
+  const EditButtonWidget(
+      {super.key,
+      required this.onPressed,
+      this.bgColor,
+      this.textColor,
+      this.buttonText});
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
+      height: 48,
       width: double.infinity,
       child: ElevatedButton(
         onPressed: onPressed,
         style: ButtonStyle(
-          backgroundColor:
-              WidgetStateProperty.all<Color>(AppColors.darkBgColor),
-          foregroundColor:
-              WidgetStateProperty.all<Color>(AppColors.episodeColor),
+          backgroundColor: isFilled
+              ? WidgetStateProperty.all<Color>(AppColors.darkBgColor)
+              : WidgetStateProperty.all<Color>(
+                  bgColor ?? AppColors.darkBgColor),
+          foregroundColor: isFilled
+              ? WidgetStateProperty.all<Color>(AppColors.episodeColor)
+              : WidgetStateProperty.all<Color>(
+                  textColor ?? AppColors.episodeColor),
           shape: WidgetStateProperty.all<RoundedRectangleBorder>(
             RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(12),
@@ -26,9 +37,9 @@ class EditButtonWidget extends StatelessWidget {
             ),
           ),
         ),
-        child: const Text(
-          "Редактировать",
-          style: TextStyle(
+        child: Text(
+          buttonText ?? "Редактировать",
+          style: const TextStyle(
             fontSize: 16,
             fontWeight: FontWeight.w400,
           ),
